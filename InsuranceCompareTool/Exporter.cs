00ad957252ef5984dc984af8989d6325da1c930e
Haiser <12568901@qq.com>
2019-03-06 22:38:04 +0800

    abc

---
+++ InsuranceCompareTool/Exporter.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI.OpenXmlFormats.Dml;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.Streaming;
+using NPOI.XSSF.UserModel;
+namespace InsuranceCompareTool
+{
+    class Exporter
+    {
+        private const string ColNameA = "上期服务人员";
+        private const string ColNameB = "客服专员工号";
+
+        public  string   Convert(string sourceFile, string targetPath)
+        {
+
+            var filename = targetPath + "\\" + GetExportFileName() + ".xlsx";
+            
+            IWorkbook excel = new XSSFWorkbook(sourceFile);
+            
+            IWorkbook tExcel = new XSSFWorkbook();
+            
+            var tsheet = tExcel.CreateSheet("main");
+
+
+            var sheet = excel.GetSheetAt(0);
+              
+            var cols = GetColumns(sheet);
+
+            CopyRow(sheet.GetRow(sheet.FirstRowNum), tsheet, true );
+            var indexA = cols.IndexOf(ColNameA);
+            var indexB = cols.IndexOf(ColNameB);
+
+            for (int i = sheet.FirstRowNum + 1 ; i <= sheet.LastRowNum; i++)
+            {
+                var row = sheet.GetRow(i);
+                if(row.Cells.Count <= indexA || row.Cells.Count <= indexB)
+                {
+                    continue;
+                }
+                var aValue = row.Cells[indexA]?.StringCellValue;
+                var bValue = row.Cells[indexB]?.StringCellValue;
+
+                if(string.IsNullOrEmpty(aValue))
+                { 
+                    continue;
+                }
+                if(string.IsNullOrEmpty(bValue))
+                    continue;
+                if (aValue.Equals(bValue, StringComparison.CurrentCultureIgnoreCase))
+                { 
+                    continue;
+                } 
+                CopyRow(row, tsheet);
+            }
+            var file = new FileStream(filename, FileMode.CreateNew, FileAccess.Write);
+            tExcel.Write(file);
+            excel.Close();
+            tExcel.Close();
+            file.Close();
+            return filename;
+        }
+        private void CopyRow(IRow row, ISheet sheet, bool isFirstRow = false)
+        {
+            var rowIndex = isFirstRow ? sheet.LastRowNum : sheet.LastRowNum + 1;
+            var trow = sheet.CreateRow(rowIndex);  
+            foreach(ICell cell in row.Cells)
+            {
+                var ncell =   trow.CreateCell(cell.ColumnIndex); 
+                ncell.CellStyle = sheet.Workbook.CreateCellStyle();
+                ncell.CellStyle.CloneStyleFrom(cell.CellStyle);
+                ncell.SetCellType(cell.CellType);
+
+                switch(cell.CellType)
+                {
+                    case CellType.String:
+                    {
+                        ncell.SetCellValue(cell.StringCellValue);
+                        break;
+                    }
+                    case CellType.Boolean:
+                    {
+                        ncell.SetCellValue( cell.BooleanCellValue);
+                        break;
+                    }
+                    case CellType.Error:
+                    {
+                        ncell.SetCellValue(cell.ErrorCellValue);
+                        break;
+                    }
+                    case CellType.Numeric:
+                    {
+                        ncell.SetCellValue(cell.NumericCellValue);
+                        break;
+                    } 
+                    default:
+                    {
+                        ncell.SetCellValue(cell.StringCellValue);
+                        break;
+                    } 
+                }  
+                if (isFirstRow)
+                { 
+                    sheet.SetColumnWidth(cell.ColumnIndex, row.Sheet.GetColumnWidth(cell.ColumnIndex));
+                } 
+            } 
+            
+        }
+        public string GetExportFileName()
+        {
+            return DateTime.Now.ToString("yyyy-MMM-dd hh mm ss"); 
+        }
+
+        private List<String> GetColumns(ISheet sheet)
+        {
+            var items = new List<String>();
+            var row = sheet.GetRow(sheet.FirstRowNum); 
+            foreach(ICell cell in row.Cells)
+            {
+                items.Add(cell.StringCellValue);
+            } 
+            
+            return items;
+
+        }
+    }
+}
---
+++ InsuranceCompareTool/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace InsuranceCompareTool.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    { 
+        private string mTitle = "Document Comparing Tool";
+        private string mSourceFile;
+        private string mTargetFile;
+        public string SourceFile
+        {
+            get => mSourceFile;
+            set
+            {
+                mSourceFile = value;
+                RaisePropertyChanged();
+            }
+        }
+        public string TargetFile
+        {
+            get => mTargetFile;
+            set
+            {
+                mTargetFile = value;
+                RaisePropertyChanged();
+            }
+        }
+        public string Title
+        {
+            get { return mTitle; }
+            set { SetProperty(ref mTitle, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            this.SourceFile = cfa.AppSettings.Settings[nameof(SourceFile)].Value;
+            this.TargetFile = cfa.AppSettings.Settings[nameof(TargetFile)].Value;
+        }
+
+
+        public ICommand SelectSourceFileCommand
+        {
+            get { return new DelegateCommand(() =>
+            {
+                var openFileDialog = new Microsoft.Win32.OpenFileDialog()
+                {
+                    Filter = "Excel Files (*.xlsx)|*.xlsx;*.xls"
+                };
+                var result = openFileDialog.ShowDialog();
+                if (result == true)
+                {
+                    SourceFile = openFileDialog.FileName;
+                }
+            });}
+        }
+
+        public ICommand SelectTargetFileCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    var openFileDialog = new FolderBrowserDialog();
+
+                    var result = openFileDialog.ShowDialog();
+                    if (result == System.Windows.Forms.DialogResult.OK)
+                    {
+                        TargetFile  = openFileDialog.SelectedPath;
+                    }
+                });
+            }
+        }
+
+        public ICommand ExportCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    try
+                    {
+                        Exporter exporter = new Exporter();
+
+                        string file = exporter.Convert(SourceFile, TargetFile);
+                        Process.Start(file);
+                    }
+                    catch(System.InvalidOperationException e1)
+                    {
+                        MessageBox.Show("操作不能继续：文件正被使用!", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    catch(Exception ex)
+                    { 
+                        MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+                    }
+
+                });
+            }
+        }
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    var cfa = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                    cfa.AppSettings.Settings[nameof(SourceFile)].Value = SourceFile;
+                    cfa.AppSettings.Settings[nameof(TargetFile)].Value = TargetFile;
+                    cfa.Save();
+                });
+            }
+        }
+    }
+}
