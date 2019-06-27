acaa045f7de989baa021376927e4e880f8f56e98
powerhai <12568901@qq.com>
2019-06-28 07:19:56 +0800

    sdsd

---
+++ InsuranceCompareTool/Modeling/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceCompareTool.Modeling
+{
+    class Class1
+    {
+    }
+}
